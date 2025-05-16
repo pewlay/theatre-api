e902f3847f1f706f7748880465f9e50572dedc50
Polina Haltseva <polinayut@gmail.com>
2025-05-16 15:18:45 +0300

    Update urls in server

--- theatre_service/theatre_service/urls.py
+++ theatre_service/theatre_service/urls.py
@@ -2,11 +2,13 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
-from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, SpectacularRedocView
+from drf_spectacular.views import (SpectacularAPIView,
+                                   SpectacularSwaggerView,
+                                   SpectacularRedocView)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/cinema/', include('theatre.urls', namespace='theatre')),
+    path('api/theatre/', include('theatre.urls', namespace='theatre')),
     path('api/user/', include('user.urls', namespace='user')),
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/doc/swagger/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
